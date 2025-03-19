caa13585b50e0ced9545a76cd051d770bbb68ffe
sosina14 <sosiayu14@gmail.com>
2025-03-19 17:00:56 +0300

    updates

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from .views import register, user_login, user_logout
 
+from .views import book_list, add_book, edit_book, delete_book
+
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+]
+
+urlpatterns += [  # Keep the authentication URLs
+    path('books/', book_list, name='book_list'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
