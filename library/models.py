b4dcb1c82401b9274e938fb16709bd67379d70cb
sosina14 <sosiayu14@gmail.com>
2025-03-20 23:23:48 +0300

    updates

--- library/models.py
+++ library/models.py
@@ -1,15 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 from django.contrib.auth import get_user_model
-
 from django.core.validators import MinValueValidator, MaxValueValidator
-
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    # other fields...
+from django.db.models import Avg  # Added for calculating average rating
 
 # User Roles
 ROLE_CHOICES = (
@@ -24,29 +17,31 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f"{self.username} ({self.role})"
 
-
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     genre = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     available_copies = models.IntegerField(default=1)
-    
+
     def __str__(self):
         return self.title
+
+    def average_rating(self):
+        """Calculate the average rating for the book"""
+        avg_rating = self.reviews.aggregate(avg_rating=Avg('rating'))['avg_rating']
+        return avg_rating if avg_rating else 0  # Return 0 if no reviews
 
 User = get_user_model()
 
 class BorrowedBook(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowed_books")
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     borrowed_at = models.DateTimeField(auto_now_add=True)
     returned = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user.username} borrowed {self.book.title}"
-    
-
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -57,10 +52,3 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.user.username} rated {self.book.title} {self.rating}/5"
-
-
-    def average_rating(self):
-        reviews = self.reviews.all()
-        if reviews:
-            return sum(review.rating for review in reviews) / len(reviews)
-        return 0
